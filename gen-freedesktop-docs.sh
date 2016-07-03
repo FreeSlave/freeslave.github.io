c5ac2505c08e7026e79fbcec4fdadbd6a1c95b31
FreeSlave <freeslave93@gmail.com>
2016-07-03 14:07:14 +0300

    Add docs

---
+++ gen-freedesktop-docs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+COMMONDIR=d-freedesktop
+
+if [ ! -d $COMMONDIR/source ]; then
+    mkdir -p $COMMONDIR/source
+fi
+
+if [ ! -d repos ]; then
+    mkdir -p repos
+fi
+
+REPOS=repos
+
+for repo in inilike desktopfile icontheme mimeapps
+do
+    if [ ! -d $REPOS/$repo ]; then
+        (cd $REPOS && git clone https://github.com/MyLittleRobo/$repo)
+    fi
+    
+    (cd $REPOS/$repo && git pull --ff --rebase=false origin master)
+    cp -r $REPOS/$repo/source $COMMONDIR/
+done
+
+(cd $COMMONDIR && dub build -b ddox)
